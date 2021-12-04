33d6ce9fbcb227f8920743fd48923baa746c838b
bartequa <bartequa@gmail.com>
2021-12-04 14:49:38 -0500

    first commit

--- OneDrive/Desktop/work-day-challenge/index.js
+++ OneDrive/Desktop/work-day-challenge/index.js
@@ -1,1 +1,8 @@
 console.log("javescripts loaded")
+// with jQuery, grab all time-blocks [9am-time-block, 10am-time-block, ...]
+// use moment to get the current hour
+// for each element, compare it against current time and add appropriate class
+
+// if there's items in localstorage, add values to corresponding hour element
+
+// event listener for saving text to localstorage
